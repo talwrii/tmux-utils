2e8ff858e30fdd4a7ab04ca39bda18aa1e589946
Tom Wright <git@tatw.name>
2016-04-04 20:08:30 -0600

    tmux-win-in-session.py -- make idempotent

--- tmux-win-in-session.py
+++ tmux-win-in-session.py
@@ -13,4 +13,9 @@ args = PARSER.parse_args()
 
 server = tmuxp.Server()
 session, = [s for s in server.list_sessions() if s['session_name'] == args.session]
-session.new_window(args.window)
+windows = [w for w in session.list_windows() if w['window_name'] == args.window]
+
+if windows:
+    session.select_window(w['window_name'])
+else:
+    session.new_window(args.window)
